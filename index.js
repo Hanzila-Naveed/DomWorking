dc6002a02eefe3f6d7e110b3b821779d3cbb8fd3
hanzila-naveed <hanzila.naveed@intagleo.com>
2023-09-05 17:45:07 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("Form")
+const Add = document.getElementById("Add")
+const AddElement = () => {
+    console.log("as")
+    const duplicateForm = document.importNode(form, true);
+    document.getElementById("form-control").appendChild(duplicateForm);
+}
+Add.onclick = () => AddElement();
+let Switch = false;
+let toggleButton = document.getElementById("toggle")
+let toggleSwitchCircle = document.getElementById("circle")
+toggleButton.onclick = () => toggleSwitchTransformFunction();
+
+function toggleSwitchTransformFunction() {
+    if (!Switch) {
+        Switch = true
+        toggleSwitchCircle.style.transform = "translateX(100%)"
+        document.body.classList.add("dark-mode");
+    } else {
+        Switch = false
+        toggleSwitchCircle.style.transform = "translateX(0%)"
+        document.body.classList.remove("dark-mode")
+    }
+
+
+}
